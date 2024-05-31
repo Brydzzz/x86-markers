b49ef8ca7115890adb392a0deb13c6958455b75a
bsilawko <01187281@pw.edu.pl>
2024-05-31 19:00:11 +0200

    updated for any size

--- main.cpp
+++ main.cpp
@@ -5,15 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 
-// #define fname "./test_bmps/example_markers.bmp"
-
-#define HEIGHT 240
-#define WIDTH 320
-
-#define HEADER_SIZE 54
 #define MAX_NUMBER_OF_MARKERS 50
 
-extern "C" int find_markers(char *bitmap, unsigned int *x_pos, unsigned int *y_pos);
+extern "C" int find_markers(unsigned char *bitmap, unsigned int *x_pos, unsigned int *y_pos);
 
 int main(int argc, char *argv[])
 {
@@ -28,38 +22,57 @@ int main(int argc, char *argv[])
         std::cout << "Too many arguments\n";
         return -1;
     }
-    fname = argv[1];
-    std::ifstream fileHandler;
-    fileHandler.open(fname, std::ios::binary);
-    if (fileHandler.good())
-        std::cout << "File opened succesfully\n";
-    else
+
+    int header_size = 54 * sizeof(unsigned char);
+    unsigned char *header;
+    FILE *fptr;
+    fptr = fopen(argv[1], "r");
+
+    if (fptr == NULL)
     {
-        std::cout << "Unable to open file\n";
-        return -2;
+        std::cout << "Invalid filne name\n";
+        return 0;
     }
 
-    // read header
-    char header[HEADER_SIZE];
-    fileHandler.read(header, HEADER_SIZE);
+    header = (unsigned char *)malloc(header_size);
+    fgets((char *)header, header_size, fptr);
+    fclose(fptr);
 
-    auto fileSize = *reinterpret_cast<uint32_t *>(&header[2]);
-    auto dataOffset = *reinterpret_cast<uint32_t *>(&header[10]);
-    auto width = *reinterpret_cast<uint32_t *>(&header[18]);
-    auto height = *reinterpret_cast<uint32_t *>(&header[22]);
+    if (header[0] != 0x42 || header[1] != 0x4D)
+    {
+        std::cout << "Wrong file format (only 24bit bitmap is supported)\n";
+        return -1;
+    }
 
-    uint32_t numOfPix = HEIGHT * WIDTH;
-    fileHandler.seekg(dataOffset); // change current read postion
-    char image[3 * numOfPix];
-    fileHandler.read(image, 3 * numOfPix);
+    unsigned int width = 0, height = 0, file_size = 0;
+
+    width += (unsigned int)header[18]; // starts at field 0x12
+    width += (unsigned int)header[19] * (1 << 8);
+
+    height += (unsigned int)header[22]; // starts at field 0x16
+    height += (unsigned int)header[23] * (1 << 8);
+
+    file_size += (unsigned int)header[2]; // starts at field 0x02
+    file_size += (unsigned int)header[3] * (1 << 8);
+    file_size += (unsigned int)header[4] * (1 << 16);
+    file_size += (unsigned int)header[5] * (1 << 24);
+
+    std::cout << "Size of the bitmap: " << width << " x " << height << std::endl;
+
+    std::cout << "File size (in bytes): " << file_size << std::endl;
+
+    unsigned char *image = (unsigned char *)malloc(file_size * sizeof(unsigned char));
+    fptr = fopen(argv[1], "r");
+    fgets((char *)image, file_size, fptr);
+    fclose(fptr);
 
     // declare arrays to store x and y coordinates of detected markers
     unsigned int x_positions[MAX_NUMBER_OF_MARKERS];
     unsigned int y_positions[MAX_NUMBER_OF_MARKERS];
 
-    int numOfMarkers = find_markers(image, x_positions, y_positions);
-    // std::cout << numOfMarkers << std::endl;
-    for (int i = 0; i < numOfMarkers; ++i)
+    int num_of_markers = find_markers((unsigned char *)image, x_positions, y_positions);
+    std::cout << "Number of markers: " << num_of_markers << std::endl;
+    for (int i = 0; i < num_of_markers; ++i)
     {
         std::cout << "Marker no." << i + 1 << " at row:" << y_positions[i] << " and column: " << x_positions[i] << '\n';
     }
